a389c3f46e8d8c319c46f0c48ed8c4c40b8ff40f
gen7919 <41898282+github-actions[bot]@users.noreply.github.com>
2026-02-07 15:09:52 +0100

    Backup automatico 2026-02-07 15:09

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 3001;
+const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('GENE1799 Backend Running!');
@@ -9,4 +9,5 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log('Server running on port ' + PORT);
 });
+
 
